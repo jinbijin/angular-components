b93328b36fe5c8e69ff9d372d73719233c35362f
Jinbi Jin <jinbijin@jinbijin.nl>
2021-06-05 19:15:56 +0000

    Fix expect typing

--- projects/angular-components/src/lib/angular-components.component.spec.ts
+++ projects/angular-components/src/lib/angular-components.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AngularComponentsComponent } from './angular-components.component';
+import * as expect from 'expect';
 
 describe('AngularComponentsComponent', () => {
   let component: AngularComponentsComponent;
--- projects/angular-components/src/lib/angular-components.service.spec.ts
+++ projects/angular-components/src/lib/angular-components.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
-
 import { AngularComponentsService } from './angular-components.service';
+import * as expect from 'expect';
 
 describe('AngularComponentsService', () => {
   let service: AngularComponentsService;
