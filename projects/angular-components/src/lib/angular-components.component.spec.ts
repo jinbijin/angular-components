a811269f039af7a272681005d936231780723e81
Jinbi Jin <jinbijin@jinbijin.nl>
2021-06-05 21:14:38 +0000

    Set up ESLint

--- projects/angular-components/src/lib/angular-components.component.spec.ts
+++ projects/angular-components/src/lib/angular-components.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { AngularComponentsComponent } from './angular-components.component';
 import * as expect from 'expect';
+import { AngularComponentsComponent } from './angular-components.component';
 
 describe('AngularComponentsComponent', () => {
   let component: AngularComponentsComponent;
--- projects/angular-components/src/lib/angular-components.service.spec.ts
+++ projects/angular-components/src/lib/angular-components.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
-import { AngularComponentsService } from './angular-components.service';
 import * as expect from 'expect';
+import { AngularComponentsService } from './angular-components.service';
 
 describe('AngularComponentsService', () => {
   let service: AngularComponentsService;
